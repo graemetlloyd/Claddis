7d2966b34211675825ac5c7883381cb44c8c8f76
graemetlloyd <graemetlloyd@gmail.com>
2014-11-26 11:03:21 +1100

    Added conditional for case where all states are missing or gaps.

--- R/AncStateEstMatrix.R
+++ R/AncStateEstMatrix.R
@@ -43,148 +43,159 @@ AncStateEstMatrix <- function(morph.matrix, tree, estimate.allchars=FALSE, estim
 
     # Get maximum value for character:
     maxval <- morph.matrix$max.vals[i]
+
+    # Case that not all characters are NA:
+    if(!is.na(minval) && !is.na(maxval)) {
     
-    # Only proceed if character is variable (non-constant):
-    if(maxval != minval) {
+      # Only proceed if character is variable (non-constant):
+      if(maxval != minval) {
             
-      # If estimating states for all taxa then treat missing values as all possible states:
-      if(estimate.allchars) morph.matrix$matrix[grep(TRUE, is.na(morph.matrix$matrix[, i])), i] <- paste(minval:maxval, collapse="&")
+        # If estimating states for all taxa then treat missing values as all possible states:
+        if(estimate.allchars) morph.matrix$matrix[grep(TRUE, is.na(morph.matrix$matrix[, i])), i] <- paste(minval:maxval, collapse="&")
             
-      # Find tips which cannot be used due to missing data:
-      tipstogo <- rownames(morph.matrix$matrix)[grep(TRUE, is.na(morph.matrix$matrix[, i]))]
+        # Find tips which cannot be used due to missing data:
+        tipstogo <- rownames(morph.matrix$matrix)[grep(TRUE, is.na(morph.matrix$matrix[, i]))]
             
-      # Only continue if at least three tips in pruned tree:
-      if(length(tipstogo) < (Ntip(tree) - 2)) {
+        # Only continue if at least three tips in pruned tree:
+        if(length(tipstogo) < (Ntip(tree) - 2)) {
       
-        # If there are tips to remove create a pruned tree:
-        if(length(tipstogo) > 0) chartree <- drop.tip(tree, tipstogo)
+          # If there are tips to remove create a pruned tree:
+          if(length(tipstogo) > 0) chartree <- drop.tip(tree, tipstogo)
         
-        # If there are no tips to remove just use full tree:
-        if(length(tipstogo) == 0) chartree <- tree
+          # If there are no tips to remove just use full tree:
+          if(length(tipstogo) == 0) chartree <- tree
             
-        # Get tip values for the pruned tree:
-        tipvals <- morph.matrix$matrix[chartree$tip.label, i]
+          # Get tip values for the pruned tree:
+          tipvals <- morph.matrix$matrix[chartree$tip.label, i]
                 
-        # Continue if not gap-coded (or apparently gap-coded) or is unordered:
-        if(!any(diff(sort(as.numeric(unlist(strsplit(tipvals, "&"))))) > 1) || morph.matrix$ordering[i] == "unord") {
+          # Continue if not gap-coded (or apparently gap-coded) or is unordered:
+          if(!any(diff(sort(as.numeric(unlist(strsplit(tipvals, "&"))))) > 1) || morph.matrix$ordering[i] == "unord") {
                     
-          # Set discrete character estimation model if unordered and or binary character:
-          if(maxval - minval == 1 || maxval - minval > 1 && morph.matrix$ordering[i] == "unord") mymodel <- "ER"
+            # Set discrete character estimation model if unordered and or binary character:
+            if(maxval - minval == 1 || maxval - minval > 1 && morph.matrix$ordering[i] == "unord") mymodel <- "ER"
                     
-          # Set discrete character estimation model if ordered multistate character:
-          if(maxval - minval > 1 && morph.matrix$ordering[i] == "ord") {
+            # Set discrete character estimation model if ordered multistate character:
+            if(maxval - minval > 1 && morph.matrix$ordering[i] == "ord") {
                         
-            # Create all zero matrix:
-            mymodel <- matrix(0, nrow=(maxval - minval) + 1, ncol=(maxval - minval) + 1)
+              # Create all zero matrix:
+              mymodel <- matrix(0, nrow=(maxval - minval) + 1, ncol=(maxval - minval) + 1)
                         
-            # Name rows and columns as states:
-            rownames(mymodel) <- colnames(mymodel) <- minval:maxval
+              # Name rows and columns as states:
+              rownames(mymodel) <- colnames(mymodel) <- minval:maxval
                         
-            # Enter one for all the off-diagonal diagonals (an ordered change model):
-            for(j in 1:(length(mymodel[1, ]) - 1)) mymodel[j + 1, j] <- mymodel[j, j + 1] <- 1
+              # Enter one for all the off-diagonal diagonals (an ordered change model):
+              for(j in 1:(length(mymodel[1, ]) - 1)) mymodel[j + 1, j] <- mymodel[j, j + 1] <- 1
 
-          }
+            }
                     
-          # Create matrix to store probabilities of tip values:
-          tipvals.mat <- matrix(0, nrow=length(tipvals), ncol=maxval - minval + 1)
+            # Create matrix to store probabilities of tip values:
+            tipvals.mat <- matrix(0, nrow=length(tipvals), ncol=maxval - minval + 1)
       
-          # Add rownames (tip labels):
-          rownames(tipvals.mat) <- names(tipvals)
+            # Add rownames (tip labels):
+            rownames(tipvals.mat) <- names(tipvals)
 
-          # Add colunames (state values):
-          colnames(tipvals.mat) <- minval:maxval
+            # Add colunames (state values):
+            colnames(tipvals.mat) <- minval:maxval
                     
-          # Fill all probabilities equal to one (non-polymorphisms):
-          for(j in colnames(tipvals.mat)) tipvals.mat[grep(TRUE, tipvals == j), j] <- 1
+            # Fill all probabilities equal to one (non-polymorphisms):
+            for(j in colnames(tipvals.mat)) tipvals.mat[grep(TRUE, tipvals == j), j] <- 1
                     
-          # If there are polymorphisms make all observed states equally probable:
-          if(any(apply(tipvals.mat, 1, sum) == 0)) {
+            # If there are polymorphisms make all observed states equally probable:
+            if(any(apply(tipvals.mat, 1, sum) == 0)) {
                         
-            # Get list of tip values with polymorphisms:
-            polymorphism.values <- grep(TRUE, apply(tipvals.mat, 1, sum) == 0)
+              # Get list of tip values with polymorphisms:
+              polymorphism.values <- grep(TRUE, apply(tipvals.mat, 1, sum) == 0)
                         
-            # Go through each polymorphism:
-            for(j in polymorphism.values) {
+              # Go through each polymorphism:
+              for(j in polymorphism.values) {
                             
-              # Get list of each state:
-              states <- strsplit(tipvals[j], "&")[[1]]
+                # Get list of each state:
+                states <- strsplit(tipvals[j], "&")[[1]]
                             
-              # Make each state equally probable in tip values matrix:
-              tipvals.mat[j, states] <- 1 / length(states)
+                # Make each state equally probable in tip values matrix:
+                tipvals.mat[j, states] <- 1 / length(states)
+
+              }
 
             }
 
-          }
-
-          # Get likelihoods for each state in taxa and ancestors:
-          state.likelihoods <- rerootingMethod(chartree, tipvals.mat, model=mymodel)$marginal.anc
+            # Get likelihoods for each state in taxa and ancestors:
+            state.likelihoods <- rerootingMethod(chartree, tipvals.mat, model=mymodel)$marginal.anc
                     
-          # Get maximum likelihood:
-          max.lik <- apply(state.likelihoods, 1, max)
+            # Get maximum likelihood:
+            max.lik <- apply(state.likelihoods, 1, max)
                     
-          # Create vector to store maximum likelihood states:
-          max.lik.state <- vector(mode="character", length=length(rownames(state.likelihoods)))
+            # Create vector to store maximum likelihood states:
+            max.lik.state <- vector(mode="character", length=length(rownames(state.likelihoods)))
       
-          # Add names:
-          names(max.lik.state) <- rownames(state.likelihoods)
+            # Add names:
+            names(max.lik.state) <- rownames(state.likelihoods)
                     
-          # For each tip and node find most likely state(s):
-          for(j in 1:length(max.lik)) max.lik.state[j] <- paste(colnames(state.likelihoods)[grep(TRUE, state.likelihoods[j, ] == max.lik[j])], collapse="&")
+            # For each tip and node find most likely state(s):
+            for(j in 1:length(max.lik)) max.lik.state[j] <- paste(colnames(state.likelihoods)[grep(TRUE, state.likelihoods[j, ] == max.lik[j])], collapse="&")
 
-        # Case if gap-coded or apparently gap-coded:
-        } else {
+          # Case if gap-coded or apparently gap-coded:
+          } else {
                     
-          # If there are polymorphisms present:
-          if(length(grep("&", tipvals)) > 0) {
+            # If there are polymorphisms present:
+            if(length(grep("&", tipvals)) > 0) {
                         
-            # List rows with polymorphisms:
-            rows <- grep("&", tipvals)
+              # List rows with polymorphisms:
+              rows <- grep("&", tipvals)
                         
-            # For each polymorphism:
-            for(j in 1:length(rows)) {
+              # For each polymorphism:
+              for(j in 1:length(rows)) {
                             
-              # Replace polymorphisms with mean values:
-              tipvals[rows[j]] <- mean(as.numeric(strsplit(tipvals[rows[j]], "&")[[1]]))
+                # Replace polymorphisms with mean values:
+                tipvals[rows[j]] <- mean(as.numeric(strsplit(tipvals[rows[j]], "&")[[1]]))
       
+              }
+
             }
+                    
+            # Treat character as continuous to get ancestral state estimates:
+            max.lik.state <- round(ace(as.numeric(tipvals), chartree)$ace)
 
           }
-                    
-          # Treat character as continuous to get ancestral state estimates:
-          max.lik.state <- round(ace(as.numeric(tipvals), chartree)$ace)
-
-        }
                 
-        # Get node numbers for pruned tree:
-        chartreenodes <- (Ntip(chartree) + 1):(Ntip(chartree) + Nnode(chartree))
+          # Get node numbers for pruned tree:
+          chartreenodes <- (Ntip(chartree) + 1):(Ntip(chartree) + Nnode(chartree))
                 
-        # For each node on pruned tree:
-        for(j in chartreenodes) {
+          # For each node on pruned tree:
+          for(j in chartreenodes) {
                     
-          # Find descendants in pruned tree:
-          descs <- sort(chartree$tip.label[FindDescendants(j, chartree)])
+            # Find descendants in pruned tree:
+            descs <- sort(chartree$tip.label[FindDescendants(j, chartree)])
                     
-          # Store ancestral values in state estimation matrix:
-          anc.lik.matrix[as.character(FindAncestor(descs, tree)), i] <- max.lik.state[as.character(j)]
+            # Store ancestral values in state estimation matrix:
+            anc.lik.matrix[as.character(FindAncestor(descs, tree)), i] <- max.lik.state[as.character(j)]
  
-        }
+          }
 
-        # If also estimating tip states:
-        if(estimate.tips == TRUE) {
+          # If also estimating tip states:
+          if(estimate.tips == TRUE) {
           
-          # Store tip states for taxa:
-          anc.lik.matrix[chartree$tip.label, i] <- max.lik.state[match(chartree$tip.label, names(max.lik.state))]
+            # Store tip states for taxa:
+            anc.lik.matrix[chartree$tip.label, i] <- max.lik.state[match(chartree$tip.label, names(max.lik.state))]
 
+          }
+
+        # Case if pruned tree too small to inform (leaves matrix as NAs)
         }
+            
+      # Case if constant character:
+      } else {
+            
+        # Enter constant value for all nodes:
+        anc.lik.matrix[, i] <- minval
 
-      # Case if pruned tree too small to inform (leaves matrix as NAs)
       }
-            
-    # Case if constant character:
+
+    # Case if all characters are NA:
     } else {
-            
-      # Enter constant value for all nodes:
-      anc.lik.matrix[, i] <- minval
+
+      # Enter NA for all nodes:
+      anc.lik.matrix[, i] <- NA
 
     }
 
