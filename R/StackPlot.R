93e4ae8557cbb018809798810ad3f32936e3331b
graemetlloyd <graemetlloyd@gmail.com>
2015-09-07 16:53:23 +1000

    Added examples for StackPlot function.

--- R/StackPlot.R
+++ R/StackPlot.R
@@ -2,11 +2,11 @@
 #' 
 #' Plots a stack of ordination spaces representing multiple time-slices.
 #' 
-#' Default is oldest at bottom to youngest at top.
+#' Default is oldest at bottom to youngest at top. Can be used for non-Claddis data too (just a way to plot ordinations as time-slices).
 #'
 #' @param ordination_axes A matrix of the ordination axes supplied (rownames should be object names). First column should be values for first axis, second for second axis and so on.
 #' @param ages A two-column matrix of the first and last apperance dates for the taxa in the same format supplied to \link{DatePhylo}.
-#' @param groups Left as NULL for now.
+#' @param groups A vector of colours (for plotting) with for each object name.
 #' @param time_slices A vector of the boundaries for a series of time slices.
 #' @param shear A single value (0 to 1) for the degree of shearing in the stacked ordination spaces.
 #' @param x_axis The ordination axis to plot on the x-axis.
@@ -22,15 +22,52 @@
 #'
 #' @examples
 #'
-#' # Nothing yet
+#' # Create x-values that will form a grid:
+#' x <- c(c(seq(0, 100, length.out = 101), seq(0, 100, length.out = 101), seq(0, 100, length.out = 101), seq(0, 100, length.out = 101)), c(rep(20, 101), rep(40, 101), rep(60, 101), rep(80, 101)))
+#'
+#' # Create y-values that will form grid:
+#' y <- c(c(rep(20, 101), rep(40, 101), rep(60, 101), rep(80, 101)), c(seq(0, 100, length.out = 101), seq(0, 100, length.out = 101), seq(0, 100, length.out = 101), seq(0, 100, length.out = 101)))
+#'
+#' # Combine x and y values into
+#' ordination_axes <- matrix(c(x, y), ncol = 2, dimnames = list(as.list(apply(matrix(sample(LETTERS, 8 * 8 * 101, replace = TRUE), nrow = 8 * 101), 1, paste, collapse = "")), NULL))
+#'
+#' # Assign ages as though taxa range through entire interval (100-0 Ma):
+#' ages <- matrix(c(rep(100, 8 * 101), rep(0, 8 * 101)), ncol = 2, dimnames = list(as.list(rownames(ordination_axes)), as.list(c("FAD", "LAD"))))
+#'
+#' # Create five 20 million year time slices:
+#' time_slices <- seq(0, 100, length.out = 6)
+#'
+#' # Plot grid lines to show "shearing" effect is working:
+#' StackPlot(ordination_axes = ordination_axes, ages = ages, time_slices = time_slices)
+#'
+#' # Set random seed:
+#' set.seed(17)
+#'
+#' # Create random values to represent ordination axes:
+#' ordination_axes <- matrix(rnorm(10000), nrow = 100, dimnames = list(as.list(apply(matrix(sample(LETTERS, 8 * 100, replace = TRUE), nrow = 100), 1, paste, collapse = "")), NULL))
+#'
+#' # Create random first and last appearance dates for objects:
+#' ages <- matrix(as.vector(apply(matrix(runif(200, 0, 100), ncol = 2), 1, sort, decreasing = TRUE)), ncol = 2, byrow=TRUE, dimnames = list(as.list(rownames(ordination_axes)), as.list(c("FAD", "LAD"))))
+#'
+#' # Create five 20 million year long time slices:
+#' time_slices <- seq(0, 100, length.out = 6)
+#'
+#' # Define groups for objects at random ("red" and "blue"):
+#' groups <- sample(x = c("red", "blue"), size = nrow(ordination_axes), replace = TRUE)
+#'
+#' # Randomly assign objects to groups:
+#' names(groups) <- rownames(ordination_axes)
+#'
+#' # Make stacked ordination plot with convex hulls for groups:
+#' StackPlot(ordination_axes, ages, groups, time_slices)
 #'
 #' @export StackPlot
 StackPlot <- function(ordination_axes, ages, groups = NULL, time_slices, shear = 0.2, x_axis = 1, y_axis = 2) {
 
-# ages is FAD and LAD, so can assume range through for first start, but may want to change later.
+# Other options for treatment of age data than just ranges?
 # Add spaces between stacks? Could even have this be negative to allow overlapping of highly sheared plots.
-# check axes asked for exist in data
-# cushioning so points don't plot at very edges of plots.
+# Check axes asked for exist in data (top level conditional).
+# Add geologic time at left with geoscale at some point?
 
   # Maybe let user set this later, but not yet:
   plot_cushion <- 0.1
@@ -40,8 +77,6 @@ StackPlot <- function(ordination_axes, ages, groups = NULL, time_slices, shear =
 
   # Record the number of stackes to plot:
   N_stacks <- length(time_bins) - 1
-
-  # Add geologic time at left with geoscale at some point?
 
   # Define x-axis label:
   xlab <- paste("PC", x_axis, " (", round((apply(ordination_axes, 2, var) / sum(apply(ordination_axes, 2, var)) * 100)[x_axis], 2), "% of total variance)", sep = "")
@@ -101,21 +136,22 @@ StackPlot <- function(ordination_axes, ages, groups = NULL, time_slices, shear =
     points_to_plot[, 2] <- (points_to_plot[, 2] * (max_y - min_y)) + min_y
 
     # Draw bounding lines for ith stack:
-    lines(x = c(0, shear * 100, 100, 100 - (shear * 100), 0), y = c((100 / N_stacks) * (i - 1), (100 / N_stacks) * i, (100 / N_stacks) * i, (100 / N_stacks) * (i - 1), (100 / N_stacks) * (i - 1)), col = "black")
+    polygon(x = c(0, shear * 100, 100, 100 - (shear * 100), 0), y = c((100 / N_stacks) * (i - 1), (100 / N_stacks) * i, (100 / N_stacks) * i, (100 / N_stacks) * (i - 1), (100 / N_stacks) * (i - 1)), col = "white")
 
     # 0, 0 lines?
 
     # Tick marks?
     
-    
     # Case if groups are specified:
     if(!is.null(groups)) {
       
-      # Plot convex hulls
-      
+      # For each group:
       for(j in unique(groups)) {
+          
+        # Get edge points (used to plot convex hull):
         edge_points <- names(which(groups[rownames(points_to_plot)] == j))[chull(x = points_to_plot[which(groups[rownames(points_to_plot)] == j), 1], y = points_to_plot[which(groups[rownames(points_to_plot)] == j), 2])]
-        edge_points <- c(edge_points, edge_points[1])
+        
+        # Plot convex hull as polygon:
         polygon(x = points_to_plot[edge_points, 1], y = points_to_plot[edge_points, 2], col = adjustcolor(j, alpha.f = 0.3) , border = 0)
         
       }
@@ -138,26 +174,3 @@ StackPlot <- function(ordination_axes, ages, groups = NULL, time_slices, shear =
   
 }
 
-ordination_axes <- matrix(rnorm(10000), nrow = 100)
-rownames(ordination_axes) <- apply(matrix(sample(LETTERS, 1000, replace = TRUE), nrow = 100), 1, paste, collapse = "")
-ages <- t(apply(matrix(runif(200, 0, 100), ncol = 2), 1, sort, decreasing = TRUE))
-colnames(ages) <- c("FAD", "LAD")
-rownames(ages) <- rownames(ordination_axes)
-time_slices <- seq(0, 100, length.out = 6)
-groups <- sample(x = c("red", "blue"), size = nrow(ordination_axes), replace = TRUE)
-names(groups) <- rownames(ordination_axes)
-
-StackPlot(ordination_axes, ages, groups, time_slices, shear = 0.2, x_axis = 1, y_axis = 2)
-
-x <- c(c(seq(0, 100, length.out = 101), seq(0, 100, length.out = 101), seq(0, 100, length.out = 101), seq(0, 100, length.out = 101)), c(rep(20, 101), rep(40, 101), rep(60, 101), rep(80, 101)))
-y <- c(c(rep(20, 101), rep(40, 101), rep(60, 101), rep(80, 101)), c(seq(0, 100, length.out = 101), seq(0, 100, length.out = 101), seq(0, 100, length.out = 101), seq(0, 100, length.out = 101)))
-ordination_axes <- cbind(x, y)
-rownames(ordination_axes) <- apply(matrix(sample(LETTERS, 8 * 8 * 101, replace = TRUE), nrow = 8 * 101), 1, paste, collapse = "")
-ages <- cbind(rep(100, 8 * 101), rep(0, 8 * 101))
-colnames(ages) <- c("FAD", "LAD")
-rownames(ages) <- rownames(ordination_axes)
-time_slices <- seq(0, 100, length.out = 6)
-groups <- sample(x = c("red", "blue"), size = nrow(ordination_axes), replace = TRUE)
-names(groups) <- rownames(ordination_axes)
-
-StackPlot(ordination_axes, ages, groups = groups, time_slices, shear = 0.2, x_axis = 1, y_axis = 2)
