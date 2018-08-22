5df1e3ab8db93560c12f58e6b4c2c9435dbb21b6
Graeme T. Lloyd <graemetlloyd@gmail.com>
2018-08-22 14:07:07 +0200

    Cut down reference list for package.

--- R/Claddis-package.R
+++ R/Claddis-package.R
@@ -14,17 +14,7 @@
 #'
 #' @references
 #'
-#' Brusatte, S. L., Benton, M. J., Ruta, M. and Lloyd, G. T., 2008a. Superiority, competition, and opportunism in the evolutionary radiation of dinosaurs. Science, 321, 1485-1488.
-#' 
-#' Brusatte, S. L., Benton, M. J., Ruta, M. and Lloyd, G. T., 2008b. The first 50 million years of dinosaur evolution: macroevolutionary pattern and morphological disparity. Biology Letters, 4, 733-736.
-#' 
-#' Brusatte, S. L., Lloyd, G. T., Wang, S. C. and Norell, M. A., 2014. Gradual assembly of avian body plan culminated in rapid rates of evolution across dinosaur-bird transition. Current Biology, 24, 2386-2392.
-#'
-#' Close, R. A., Friedman, M., Lloyd, G. T. and Benson, R. B. J., 2015. Evidence for a mid-Jurassic adaptive radiation in mammals. Current Biology, 25, 2137-2142.
-#'
-#' Lloyd, G. T., 2016. Estimating morphological diversity and tempo with discrete character-taxon matrices: implementation, challenges, progress, and future directions. Biological Journal of the Linnean Society, 118, 131-151..
-#'
-#' Lloyd, G. T., Wang, S. C. and Brusatte, S. L., 2012. Identifying heterogeneity in rates of morphological evolution: discrete character change in the evolution of lungfish (Sarcopterygii; Dipnoi). Evolution, 66, 330-348.
+#' Lloyd, G. T., 2016. Estimating morphological diversity and tempo with discrete character-taxon matrices: implementation, challenges, progress, and future directions. Biological Journal of the Linnean Society, 118, 131-151.
 #'
 #' @keywords disparity,distance,morphology,phylogeny
 #'
