a3a6a81818742667438dc82c2d8c6786c0a074c3
Graeme T. Lloyd <graemetlloyd@gmail.com>
2018-08-27 12:48:42 +0100

    Rebuilt matrix pruner for new format.

--- R/MatrixPruner.R
+++ R/MatrixPruner.R
@@ -1,12 +1,14 @@
 #' Prunes a character matrix of characters or taxa
-#' 
+#'
 #' Prunes a character matrix of characters, taxa, or both.
-#' 
-#' Removing characters or taxa from a matrix imported using \link{ReadMorphNexus} is not simple due to associated vectors for ordering, character weights etc. To save repetitively pruning each part this function takes the matrix as input and vectors of either taxon names, character numbers, or both and removes those from the matrix. Minimum and maximum values (used by \link{MorphDistMatrix}) are also updated.
-#' 
+#'
+#' Removing characters or taxa from a matrix imported using \link{ReadMorphNexus} is not simple due to associated vectors for ordering, character weights etc. To save repetitively pruning each part this function takes the matrix as input and vector(s) of either block numbers, character numbers, taxon names, or any combination thereof and returns a matrix with these items removed. Minimum and maximum values (used by \link{MorphDistMatrix}) are also updated and the user has the option to remove constant characters this way as well (e.g, to reduce the memory required for a DNA matrix).
+#'
 #' @param clad.matrix The cladistic matrix in the format imported by \link{ReadMorphNexus}.
-#' @param taxa2prune A vector of taxon names to prune (these must be present in \code{rownames(clad.matrix$matrix}).
+#' @param blocks2prune A vector of number(s) of any blocks to prune.
 #' @param characters2prune A vector of character numbers to prune.
+#' @param taxa2prune A vector of taxon names to prune (these must be present in \code{rownames(clad.matrix$matrix}).
+#' @param removeinvariant A logical for whether invariant characters should (TRUE) or should not (FALSE, default) be pruned.
 #'
 #' @author Graeme T. Lloyd \email{graemetlloyd@@gmail.com}
 #'
@@ -15,66 +17,152 @@
 #' @keywords NEXUS
 #'
 #' @examples
-#' 
+#'
 #' # Remove the outgroup taxon and characters 11 and 53 from Gauthier1986:
-#' prunedmatrix <- MatrixPruner(clad.matrix = Gauthier1986, taxa2prune = c("Outgroup"),
-#'   characters2prune = c(11, 53))
-#' 
+#' prunedmatrix <- MatrixPruner(clad.matrix = Gauthier1986, characters2prune = c(11, 53),
+#'   taxa2prune = c("Outgroup"))
+#'
+#' # Show priuned matrix:
+#' prunedmatrix$Matrix_1$Matrix
+#'
 #' @export MatrixPruner
-MatrixPruner <- function(clad.matrix, taxa2prune = c(), characters2prune = c()) {
-
-# ADD OPTION TO REMOVE CONSTANT CHARACTERS
-
-    # Check that something to prune has been specified:
-    if(is.null(taxa2prune) && is.null(characters2prune)) stop("No taxa or characters to prune specified.")
-
-    # Check taxa to prune have names matching those in matrix:
-    if(length(setdiff(taxa2prune, rownames(clad.matrix$matrix))) > 0) stop("Taxa specified that are not found in the matrix. Check spelling.")
-
-    # Check character numbers are present in matrix:
-    if(length(setdiff(characters2prune, c(1:ncol(clad.matrix$matrix))))) stop("Character numbers specified that are not found in the matrix.")
-
-    # If there are taxa to prune, then prune them:
-    if(!is.null(taxa2prune)) clad.matrix$matrix <- clad.matrix$matrix[-match(taxa2prune, rownames(clad.matrix$matrix)), , drop = FALSE]
-
-    # If there are characters to prune:
-    if(!is.null(characters2prune)) {
-        
-        # Remove from matrix
-        clad.matrix$matrix <- clad.matrix$matrix[, -characters2prune, drop = FALSE]
-        
-        # Remove from ordering:
-        clad.matrix$ordering <- clad.matrix$ordering[-characters2prune]
-        
-        # Remove from weights:
-        clad.matrix$weights <- clad.matrix$weights[-characters2prune]
-        
-        # Remove from maximum values:
-        clad.matrix$max.vals <- clad.matrix$max.vals[-characters2prune]
-        
-        # Remove from minimum values:
-        clad.matrix$min.vals <- clad.matrix$min.vals[-characters2prune]
-        
+MatrixPruner <- function(clad.matrix, blocks2prune = c(), characters2prune = c(), taxa2prune = c(), removeinvariant = FALSE) {
+  
+  # Subfunction to find length of character types for each character (i.e., unique values excluding polymorphisms but included inapplicables):
+  LengthFinder <- function(x) {
+    
+    # Convert each column of matrix to a list of numeric values:
+    x <- lapply(lapply(lapply(lapply(lapply(lapply(apply(x, 2, as.list), unlist), strsplit, split = "&|/"), unlist), unique), sort), length)
+    
+    # Return(x):
+    return(x)
+    
+  }
+  
+  # Check that something to prune has been specified:
+  if(is.null(blocks2prune) && is.null(characters2prune) && is.null(taxa2prune) && removeinvariant == FALSE) stop("No blocks, taxa, or characters to prune specified.")
+  
+  # Check blocks specified exist and stop and warn :
+  if(length(setdiff(blocks2prune, 1:(length(clad.matrix) - 1))) > 0) stop("Block numbers specified that are not present in data.")
+  
+  # Check characters specified exist and stop and warn user if not:
+  if(length(setdiff(characters2prune, 1:sum(unlist(lapply(lapply(clad.matrix[2:length(clad.matrix)], '[[', "Matrix"), ncol))))) > 0) stop("Characters specified that are outside the scope of the matrix. Check and retry.")
+  
+  # Check taxa specified exist and stop and warn user if not:
+  if(length(setdiff(taxa2prune, rownames(clad.matrix$Matrix_1$Matrix))) > 0) stop("Taxa specified that are not found in the matrix. Check and retry.")
+  
+  # Get number of characters:
+  NChars <- sum(unlist(lapply(lapply(clad.matrix[2:length(clad.matrix)], '[[', "Matrix"), ncol)))
+  
+  # If there are characters to prune:
+  if(!is.null(characters2prune)) {
+    
+    # Get character blocks for each character in descendng rder (as want to work backwards so things match up properly):
+    CharacterBlocks <- unlist(lapply(lapply(lapply(as.list(sort(characters2prune, decreasing = TRUE)), '>', unlist(lapply(lapply(clad.matrix[2:length(clad.matrix)], '[[', "Matrix"), ncol))), which), length)) + 1
+    
+    # Initial build of characters in list form:
+    CharactersAsList <- lapply(lapply(clad.matrix[2:length(clad.matrix)], '[[', "Matrix"), function(x) 1:ncol(x))
+    
+    # Actually form list of character numbers:
+    for(i in 2:length(CharactersAsList)) CharactersAsList[[i]] <- CharactersAsList[[i]] + max(CharactersAsList[[(i - 1)]])
+    
+    # For each unique character vlock:
+    for(i in unique(CharacterBlocks)) {
+      
+      # Find columns to delete in ith matrix:
+      ColumnsToDelete <- match(sort(characters2prune, decreasing = TRUE)[CharacterBlocks == i], CharactersAsList[[i]])
+      
+      # Remove characters from matrix:
+      clad.matrix[[(i + 1)]]$Matrix <- clad.matrix[[(i + 1)]]$Matrix[, -ColumnsToDelete, drop = FALSE]
+      
+      # Remove characters from ordering:
+      clad.matrix[[(i + 1)]]$Ordering <- clad.matrix[[(i + 1)]]$Ordering[-ColumnsToDelete]
+      
+      # Remove characters from weights:
+      clad.matrix[[(i + 1)]]$Weights <- clad.matrix[[(i + 1)]]$Weights[-ColumnsToDelete]
+      
+      # Remove characters from minimum values:
+      clad.matrix[[(i + 1)]]$MinVals <- clad.matrix[[(i + 1)]]$MinVals[-ColumnsToDelete]
+      
+      # Remove characters from maximum values:
+      clad.matrix[[(i + 1)]]$MaxVals <- clad.matrix[[(i + 1)]]$MaxVals[-ColumnsToDelete]
+      
     }
     
-    # Get unique values for each character:
-    unique.values <- lapply(lapply(lapply(lapply(lapply(lapply(lapply(apply(clad.matrix$matrix, 2, list), unlist), as.character), strsplit, split = "&"), unlist), sort), unique), as.numeric)
+  }
+  
+  # If there are taxa to prune:
+  if(!is.null(taxa2prune)) {
     
-    # If any character is now all missing data:
-    if(length(which(unlist(lapply(unique.values, length)) == 0)) > 0) {
+    # Remove pruned taxa from each block:
+    for(i in 2:length(clad.matrix)) clad.matrix[[i]]$Matrix <- clad.matrix[[i]]$Matrix[-match(taxa2prune, rownames(clad.matrix[[i]]$Matrix)), , drop = FALSE]
+    
+  }
+  
+  # If there are blocks to prune:
+  if(!is.null(blocks2prune)) {
+    
+    # Remove blocks to be rpuned:
+    clad.matrix <- clad.matrix[-(blocks2prune + 1)]
+    
+    # Rename (renumber) remaining matrix blocks:
+    names(clad.matrix[2:length(clad.matrix)]) <- paste("Matrix_", 1:(length(clad.matrix) - 1), sep = "")
+    
+  }
+  
+  # If there are invariant characters:
+  if(removeinvariant) {
+    
+    # Find any invariant characters:
+    InvariantsAsList <- lapply(lapply(lapply(lapply(lapply(clad.matrix[2:length(clad.matrix)], '[[', "Matrix"), LengthFinder), unlist), '<', 1), which)
+    
+    # If there are invariant characters:
+    if(length(unlist(InvariantsAsList)) > 0) {
+      
+      # For each matrix block:
+      for(i in 1:length(InvariantsAsList)) {
         
-        # For each such character insert a single zero:
-        for(i in which(unlist(lapply(unique.values, length)) == 0)) unique.values[[i]] <- c(0)
+        # Only if there are invraints for this block:
+        if(length(InvariantsAsList[[i]]) > 0) {
+          
+          # Remove characters from matrix:
+          clad.matrix[[(i + 1)]]$Matrix <- clad.matrix[[(i + 1)]]$Matrix[, -InvariantsAsList[[i]], drop = FALSE]
+          
+          # Remove characters from ordering:
+          clad.matrix[[(i + 1)]]$Ordering <- clad.matrix[[(i + 1)]]$Ordering[-InvariantsAsList[[i]]]
+          
+          # Remove characters from weights:
+          clad.matrix[[(i + 1)]]$Weights <- clad.matrix[[(i + 1)]]$Weights[-InvariantsAsList[[i]]]
+          
+          # Remove characters from minimum values:
+          clad.matrix[[(i + 1)]]$MinVals <- clad.matrix[[(i + 1)]]$MinVals[-InvariantsAsList[[i]]]
+          
+          # Remove characters from maximum values:
+          clad.matrix[[(i + 1)]]$MaxVals <- clad.matrix[[(i + 1)]]$MaxVals[-InvariantsAsList[[i]]]
+          
+        }
         
+      }
+      
     }
     
-    # Update maximum values (post pruning):
-    clad.matrix$max.vals <- unlist(lapply(unique.values, max))
+  }
+  
+  # Check for empty blocks and store them as blocks to delete if found:
+  NewBlocksToDelete <- which(unlist(lapply(lapply(clad.matrix[2:length(clad.matrix)], '[[', "Matrix"), ncol)) == 0)
+  
+  # If there are new blocks to prune:
+  if(length(NewBlocksToDelete) > 0) {
     
-    # Update minimum values (post pruning):
-    clad.matrix$min.vals <- unlist(lapply(unique.values, min))
-
-    # Return pruned matrix:
-    return(clad.matrix)
-
+    # Remove blocks to be rpuned:
+    clad.matrix <- clad.matrix[-(NewBlocksToDelete + 1)]
+    
+    # Rename (renumber) remaining matrix blocks:
+    names(clad.matrix[2:length(clad.matrix)]) <- paste("Matrix_", 1:(length(clad.matrix) - 1), sep = "")
+    
+  }
+  
+  # Return pruned matrix:
+  return(clad.matrix)
+  
 }
