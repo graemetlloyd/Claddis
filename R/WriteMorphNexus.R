a165f3f517db0034912f822f107b43bafeae9e97
Graeme T. Lloyd <graemetlloyd@gmail.com>
2018-09-16 20:26:13 +0100

    First proper addition of WriteMorphNEXUS for new format.

--- R/WriteMorphNexus.R
+++ R/WriteMorphNexus.R
@@ -94,6 +94,43 @@ WriteMorphNexus <- function(clad.matrix, filename) {
     
   }
   
+  # Subfunction for collapsing strings of character numbers (i.e., for weight and ordering in assumptions block):
+  Zipper <- function(x) {
+    
+    # Set up empty zipped version of strin:
+    ZippedString <- vector(mode = "character")
+    
+    # As long as there are zip-able parts to x remaining:
+    while(any(diff(x) == 1)) {
+      
+      # Find start of first zippable part:
+      StartOfZip <- which(diff(x) == 1)[1]
+      
+      # Find length of zippable part:
+      ZipLength <- rle(diff(x))$lengths[which(rle(diff(x))$values == 1)[1]]
+      
+      # Use length and start to find end of zip:
+      EndOfZip <- StartOfZip + ZipLength
+      
+      # Add zippable part to string (and any preceding unzippable parts):
+      ZippedString <- c(ZippedString, setdiff(x[1:EndOfZip], x[StartOfZip:EndOfZip]), paste(x[StartOfZip], x[EndOfZip], sep = "-"))
+      
+      # Cut x down ready to find next zippable part:
+      x <- x[-(1:EndOfZip)]
+      
+    }
+    
+    # If there are characters left (or there are no zippable parts):
+    if(length(x) > 0) ZippedString <- c(ZippedString, x)
+    
+    # Collapse to a single string with spaces:
+    ZippedString <- paste(ZippedString, collapse = " ")
+    
+    # Return zipped string:
+    return(ZippedString)
+    
+  }
+  
   # If header text is present:
   if(nchar(paste(clad.matrix$Topper$Header, collapse = "")) > 0) {
     
@@ -135,275 +172,55 @@ WriteMorphNexus <- function(clad.matrix, filename) {
   # Conver matrices to vectors of text strings:
   DataBlocksAsTextStrings <- lapply(DataBlocks, MatrixConversion)
   
-  
-  
-  
-  
-  
-  
-  
-  # Stet satrt of data block (data for single block, characters if multiple):
-  DataBlockStart <- ifelse(length(DataBlocks) > 1, "BEGIN CHARACTERS;\n", paste("BEGIN DATA;\n\tDIMENSIONS  NTAX=", NTaxa, " ", collapse = ""))
-  
-  # Reformta symbols (returning empty string for continuous) eady for data or character block(s):
-  Symbols <- unlist(lapply(as.list(Symbols), function(x) ifelse(nchar(x) > 0, paste(" SYMBOLS=\" ", x, "\" ", sep = ""), " ")))
-  
-  # Set up character and format lines:
-  CharacterAndFormatLines <- paste("NCHAR=", NCharacters, ";\n\tFORMAT ", "DATATYPE=", Datatypes, Symbols, "MISSING=", Missing, " GAP=", Gap, " ;", sep = "")
+  # Set up header block (returns empty string if nothing there):
+  HeaderBlock <- ifelse(length(clad.matrix$Topper$Header) > 0, paste("[", clad.matrix$Topper$Header, "]\n\n", sep = ""), "")
 
+  # Set up taxa block (only required if multiple matrix blocks as sets number of taxa, will be empty string otherwise):
+  TaxaBlock <- ifelse(length(DataBlocks) > 1, paste("BEGIN TAXA;\n\tDIMENSIONS NTAX=", NTaxa, ";\n\tTAXLABELS\n\t\t", paste(rownames(clad.matrix$Matrix_1$Matrix), collapse = " "), "\n;\nEND;\n\n", sep = ""), "")
+  
+  # Set up data block (only required if a single matrix block):
+  DataBlock <- ifelse(length(DataBlocks) == 1, paste("BEGIN DATA;\n\tDIMENSIONS  NTAX=", NTaxa, " NCHAR=", NCharacters, " ;\n\tFORMAT DATATYPE=", Datatypes, " SYMBOLS=\" ", Symbols, "\" MISSING=", Missing, " GAP=", Gap, " ;\n", sep = ""), "")
+  
+  # Set up character block (including MATRIX that will begin data):
+  CharacterBlock <- ifelse(rep(length(DataBlocks), length(DataBlocks)) > 1, paste("BEGIN CHARACTERS;\n\t", ifelse(nchar(BlockNames) > 0, paste("TITLE  ", BlockNames, ";\n", sep = ""), ""), "\tDIMENSIONS  NCHAR=", NCharacters, ";\n\tFORMAT  DATATYPE=", ifelse(Datatypes == "CONTINUOUS", "CONTINUOUS ", paste(Datatypes, " SYMBOLS=\"", Symbols, "\" ", sep = "")), "MISSING=", Missing, " GAP=", Gap, " ;\nMATRIX\n\n", sep = ""), "MATRIX\n\n")
+  
+  # Take character block and meld with matri(ces) into matrix block(s):
+  MatrixBlock <- paste(paste(CharacterBlock, unlist(lapply(DataBlocksAsTextStrings, paste, collapse = "\n")), "\n;\nEND;\n\n", sep = ""), collapse = "")
+  
+  # Make sure step matrices are a list if null:
+  if(!is.list(clad.matrix$Topper$StepMatrices)) clad.matrix$Topper$StepMatrices <- list(NULL)
+  
+  # Create step matrix block:
+  StepMatrixBlock <- paste(ifelse(!unlist(lapply(clad.matrix$Topper$StepMatrices, is.null)), paste(paste("\tUSERTYPE '", names(clad.matrix$Topper$StepMatrices), "' (STEPMATRIX) = ", unlist(lapply(clad.matrix$Topper$StepMatrices, ncol)), "\n", sep = ""), paste("\t", unlist(lapply(lapply(clad.matrix$Topper$StepMatrices, colnames), paste, collapse = " ")), "\n\t", sep = ""), unlist(lapply(lapply(lapply(clad.matrix$Topper$StepMatrices, function(x) { diag(x) <- "."; return(x) }), apply, 2, paste, collapse = " "), paste, collapse = "\n\t")), "\n\t;\n", sep = ""), ""), collapse = "")
 
+  # Get ordering of all characters in sequence:
+  Ordering <- unlist(lapply(DataBlocks, '[[', "Ordering"))
+  
+  # Get weights of all characters in sequence:
+  Weights <- unlist(lapply(DataBlocks, '[[', "Weights"))
 
+  # Create options block (if no block names):
+  if(all(is.na(BlockNames))) OptionsBlock <- paste(ifelse(all(Ordering == "unord"), "\tOPTIONS  DEFTYPE=unord PolyTcount=MINSTEPS ;\n", ifelse(all(Ordering == "ord"), "\tOPTIONS  DEFTYPE=ord PolyTcount=MINSTEPS ;\n", "\tOPTIONS  DEFTYPE=unord PolyTcount=MINSTEPS ;\n")), paste("\tTYPESET * UNTITLED  = ", paste(paste(sort(unique(Ordering)), unlist(lapply(lapply(lapply(as.list(sort(unique(Ordering))), '==', Ordering), which), Zipper)), sep = ": "), collapse = ", "), ";\n", sep = ""), collapse = "")
+  
+  # Create options block (if there are block names):
+  if(!all(is.na(unlist(BlockNames)))) OptionsBlock <- paste(paste("\tTYPESET * UNTITLED  (CHARACTERS = ", BlockNames, ")  =  ", unlist(lapply(lapply(DataBlocks, '[[', "Ordering"), function(x) paste(paste(paste(sort(unique(x)), unlist(lapply(lapply(lapply(as.list(sort(unique(x))), '==', x), which), Zipper)), sep = ": "), collapse = ", "), sep = ""))), ";\n", sep = ""), collapse = "")
+  
+  # Replace cont with Squared if continuous characters present:
+  if(length(grep(" cont: ", OptionsBlock)) > 0) OptionsBlock <- gsub(" cont: ", " Squared: ", OptionsBlock)
+  
+  # Convert continuosu character weights to one before making weights block:
+  Weights[Ordering == "cont"] <- 1
+  
+  # Make weights block:
+  WeightsBlock <- ifelse(all(Weights == 1), "", paste("\tWTSET * UNTITLED  = ", paste(paste(sort(unique(Weights)), unlist(lapply(lapply(lapply(as.list(sort(unique(Weights))), '==', Weights), which), Zipper)), sep = ": "), collapse = ", "), ";\n", sep = ""))
+  
+  # Build assumptions block:
+  AssumptionBlock <- paste("BEGIN ASSUMPTIONS;\n", StepMatrixBlock, OptionsBlock, WeightsBlock, "END;\n", sep = "")
+  
+  # Build full string with all blocks together:
+  FullString <- paste("#NEXUS\n\n", HeaderBlock, TaxaBlock, DataBlock, MatrixBlock, AssumptionBlock, sep = "")
 
-
-
-  # Set up taxa block (only required if multiple matrix blocks as sets number of taxa):
-  TaxaBlock <- ifelse(length(DataBlocks) > 1, paste("\tBEGIN TAXA;\n\tDIMENSIONS NTAX=", NTaxa, ";\n\tTAXLABELS\n\t\t", paste(rownames(clad.matrix$Matrix_1$Matrix), collapse = " "), "\n\t;\n\tEND;", sep = ""), "")
-  
-  # Make data block string:
-  datablock <- paste(DataBlockStart, paste("NCHAR=", NCharacters, ";", sep = ""), paste("\t", "FORMAT SYMBOLS= \" ", Symbols, "\" MISSING=", Missing, " GAP=", Gap, " ;", sep = ""), "\n", sep = "\n")
-  
-  
-  
-  
-  
-    
-  # TITLE if blocks have titles, check that single matrix with title can store this maybe in ReadMorphNexus?
-  # Removes symbols for continuous
-  # Remove ntax for multiple blocks
-  # ADD TAXA BLOCK FOR MULTIPLE BLOCK CASES
-  
-  
-  
-  
-  
-  
-  # For each state (including polymorphisms):
-  for(i in unique.states) {
-    
-    # Store positions in matrix of ith state:
-    state.positions <- which(clad.matrix$matrix == i)
-    
-    # Get state symbol:
-    ifelse(length(grep("&", i)) > 0, state.symbol <- clad.matrix$symbols[as.numeric(strsplit(i, "&")[[1]]) + 1], state.symbol <- clad.matrix$symbols[as.numeric(i) + 1])
-    
-    # If state symbol is a polymorphisms modify for writing:
-    if(length(state.symbol) > 1) state.symbol <- paste("(", paste(state.symbol, collapse = ""), ")", sep = "")
-    
-    # Write correct state symbols into the matrix block:
-    matrixblock[state.positions] <- state.symbol
-    
-  }
-  
-  # Make top of matrix block:
-  matrixblock.top <- c("MATRIX", "")
-  
-  # Get spaces required after each taxon name:
-  name.spaces <- (max(nchar(rownames(clad.matrix$matrix)))+2)-nchar(rownames(clad.matrix$matrix))
-  
-  # Make matrix strings and store as rownames:
-  for(i in 1:nrow(matrixblock)) rownames(matrixblock)[i] <- paste(rownames(matrixblock)[i], paste(rep(" ", name.spaces[i]), collapse = ""), paste(matrixblock[i, ], collapse = ""), sep = "")
-  
-  # Remake matrixblock as single string:
-  matrixblock <- paste("MATRIX\n\n", paste(rownames(matrixblock), collapse = "\n"), "\n;\nEND;\n\n", sep = "")
-  
-  # Case if all characters are unordered:
-  if(length(unique(clad.matrix$ordering)) == 1 && unique(clad.matrix$ordering) == "unord") ord.block <- "\tOPTIONS  DEFTYPE=unord PolyTcount=MINSTEPS ;"
-  
-  # Case if all characters are ordered:
-  if(length(unique(clad.matrix$ordering)) == 1 && unique(clad.matrix$ordering) == "ord") ord.block <- "\tOPTIONS  DEFTYPE=ord PolyTcount=MINSTEPS ;"
-  
-  # Case if mix of character types:
-  if(length(unique(clad.matrix$ordering)) > 1) {
-    
-    # Empty vector to store character strings:
-    character.strings <- vector(mode = "numeric")
-    
-    # Get character types (ord, unord, stepmatrix):
-    character.types <- unique(clad.matrix$ordering)
-    
-    # For each character type (unord, ord, stepmatrix):
-    for(i in character.types) {
-      
-      # Find characters of current character type:
-      character.numbers <- which(clad.matrix$ordering == i)
-      
-      # If there are contiguous character numbers:
-      if(any(diff(character.numbers) == 1)) {
-        
-        # Set up character string:
-        char.string <- paste(i, ":", sep = "")
-        
-        # Set first character number:
-        j <- character.numbers[1]
-        
-        # While we havem't reached the end:
-        while(j < character.numbers[length(character.numbers)]) {
-          
-          # What is last character in the contiguous run:
-          endrun <- character.numbers[which(character.numbers == j):length(character.numbers)][min(c(which(diff(character.numbers[which(character.numbers == j):length(character.numbers)]) > 1), length(character.numbers[which(character.numbers == j):length(character.numbers)])))]
-          
-          # If last character in run is after first character:
-          if(endrun > j) {
-            
-            # Store run by collapsing with a dash:
-            char.string <- c(char.string, paste(j, "-", endrun, sep = ""))
-            
-            # Update j to the next character (or keep as last if last character):
-            j <- character.numbers[min(c(which(character.numbers == endrun) + 1, length(character.numbers)))]
-            
-            # If the last character is also the current character (there is no contiguous run):
-          } else {
-            
-            # Store as single character:
-            char.string <- c(char.string, j)
-            
-            # Update j to next character in sequence (or keep as last character if last character):
-            j <- character.numbers[min(c(which(character.numbers == j) + 1, length(character.numbers)))]
-            
-            # Special case where last character is isolated (not part of a contiguous run):
-            if(j == character.numbers[length(character.numbers)]) char.string <- c(char.string, j)
-            
-          }
-          
-        }
-        
-        # Complete character strong:
-        char.string <- paste(char.string, collapse = " ")
-        
-        # If character numbers are all non-contiguous:
-      } else {
-        
-        # Complete character string:
-        char.string <- paste(paste(i, ":", sep = ""), paste(character.numbers, collapse = " "), collapse = " ")
-        
-      }
-      
-      # Combine character strings:
-      character.strings <- c(character.strings, char.string)
-      
-    }
-    
-    # Create ordering block:
-    ord.block <- paste("\tOPTIONS  DEFTYPE=unord PolyTcount=MINSTEPS ;\n\tTYPESET * UNTITLED  = ", paste(sort(character.strings, decreasing = TRUE), collapse = ", "), ";", sep = "")
-    
-  }
-  
-  # If different weights are specified:
-  if(length(unique(clad.matrix$weights)) > 1) {
-    
-    # Empty vector to store weight strings:
-    weight.strings <- vector(mode = "numeric")
-    
-    # Get character types (ord, unord, stepmatrix):
-    weight.values <- unique(clad.matrix$weights)
-    
-    # For each weight value:
-    for(i in weight.values) {
-      
-      # Find characters of current weight:
-      character.numbers <- which(clad.matrix$weights == i)
-      
-      # If there are contiguous character numbers:
-      if(any(diff(character.numbers) == 1)) {
-        
-        # Set up weight string:
-        weight.string <- paste(i, ":", sep = "")
-        
-        # Set first character number:
-        j <- character.numbers[1]
-        
-        # While we havem't reached the end:
-        while(j < character.numbers[length(character.numbers)]) {
-          
-          # What is last character in the contiguous run:
-          endrun <- character.numbers[which(character.numbers == j):length(character.numbers)][min(c(which(diff(character.numbers[which(character.numbers == j):length(character.numbers)]) > 1), length(character.numbers[which(character.numbers == j):length(character.numbers)])))]
-          
-          # If last character in run is after first character:
-          if(endrun > j) {
-            
-            # Store run by collapsing with a dash:
-            weight.string <- c(weight.string, paste(j, "-", endrun, sep = ""))
-            
-            # Update j to the next character (or keep as last if last character):
-            j <- character.numbers[min(c(which(character.numbers == endrun) + 1, length(character.numbers)))]
-            
-            # If the last character is also the current character (there is no contiguous run):
-          } else {
-            
-            # Store as single character:
-            weight.string <- c(weight.string, j)
-            
-            # Update j to next character in sequence (or keep as last character if last character):
-            j <- character.numbers[min(c(which(character.numbers == j) + 1, length(character.numbers)))]
-            
-            # Special case where last character is isolated (not part of a contiguous run):
-            if(j == character.numbers[length(character.numbers)]) weight.string <- c(weight.string, j)
-            
-          }
-          
-        }
-        
-        # Complete character strong:
-        weight.string <- paste(weight.string, collapse = " ")
-        
-        # If character numbers are all non-contiguous:
-      } else {
-        
-        # Complete character string:
-        weight.string <- paste(paste(i, ":", sep = ""), paste(character.numbers, collapse = " "), collapse = " ")
-        
-      }
-      
-      # Combine character strings:
-      weight.strings <- c(weight.strings, weight.string)
-      
-    }
-    
-    # Create ordering block:
-    weight.block <- paste("\tWTSET * UNTITLED  = ", paste(sort(weight.strings, decreasing = TRUE), collapse = ", "), ";", sep = "")
-    
-    # Add weights to ord block
-    ord.block <- paste(ord.block, "\n", weight.block, sep = "")
-    
-  }
-  
-  # If there are step matrices:
-  if(!is.null(clad.matrix$step.matrices)) {
-    
-    # Create empty step matrices vector:
-    step_matrices <- vector(mode = "character")
-    
-    # For each step matrix:
-    for(i in 1:length(clad.matrix$step.matrices)) {
-      
-      # Replace diagonal with a period:
-      diag(clad.matrix$step.matrices[[i]]) <- "."
-      
-      # Add ith step matrix to step matrices:
-      step_matrices[i] <- paste(c(paste("\tUSERTYPE '", names(clad.matrix$step.matrices)[i], "' (STEPMATRIX) = ", ncol(clad.matrix$step.matrices[[i]]), sep = ""), paste("\t", paste(colnames(clad.matrix$step.matrices[[i]]), collapse = " "), sep = ""), paste("\t", apply(clad.matrix$step.matrices[[i]], 1, paste, collapse = " "), sep = ""), "\t;\n"), collapse = "\n")
-      
-    }
-    
-  }
-  
-  # If there are no step matrices:
-  if(is.null(clad.matrix$step.matrices)) {
-    
-    # Finish assumptions block:
-    ass.block <- paste("BEGIN ASSUMPTIONS;\n", ord.block, "\nEND;\n", sep = "")
-    
-    # If there are step matrices:
-  } else {
-    
-    # Finish assumptions block:
-    ass.block <- paste("BEGIN ASSUMPTIONS;\n", paste(step_matrices, collapse = ""), ord.block, "\nEND;\n", collapse = "")
-    
-  }
-  
-  # Compile output:
-  out <- paste(headlines, datablock, matrixblock, ass.block, sep = "")
-  
   # Write to file:
-  write(out, filename)
-  
+  write(FullString, filename)
+
 }
