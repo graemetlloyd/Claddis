60a4679ac62f088a3d18aa506e8cfe5d5b01fe05
Graeme T. Lloyd <graemetlloyd@gmail.com>
2019-01-11 23:27:41 +0000

    Tweaked PlotCharacterChanges to deal with new rates output format.

--- R/PlotCharacterChanges.R
+++ R/PlotCharacterChanges.R
@@ -31,44 +31,46 @@
 #'
 #' # Get discrete character rates (includes changes):
 #' out <- DiscreteCharacterRate(tree, Michaux1989,
-#'   seq(tree$root.time, 0, length.out = 3), alpha = 0.01)
+#'   seq(tree$root.time, 0, length.out = 3),
+#'   BranchPartitionsToTest = list(list(1)), Alpha = 0.01)
 #'
 #' # Plot character changes on the tree:
-#' PlotCharacterChanges(out$character.changes, tree)
+#' PlotCharacterChanges(out$InferredCharacterChanges,
+#'   tree)
 #'
 #' @export PlotCharacterChanges
 PlotCharacterChanges <- function(character.changes, tree) {
-	
-    # Update tree edge lengths to number of character changes:
-    tree$edge.length <- rle(sort(c(character.changes[, "Branch"], 1:nrow(tree$edge))))$lengths - 1
+  
+  # Update tree edge lengths to number of character changes:
+  tree$edge.length <- rle(sort(c(character.changes[, "Edge"], 1:nrow(tree$edge))))$lengths - 1
+  
+  # Create empty edge labels vector:
+  edge.labels <- rep(NA, nrow(tree$edge))
+  
+  # For each edge:
+  for(i in 1:nrow(tree$edge)) {
     
-    # Create empty edge labels vector:
-    edge.labels <- rep(NA, nrow(tree$edge))
+    # Get rows for where changes occur:
+    change.rows <- which(character.changes[, "Edge"] == i)
     
-    # For each edge:
-    for(i in 1:nrow(tree$edge)) {
-        
-        # Get rows for where changes occur:
-        change.rows <- which(character.changes[, "Branch"] == i)
-        
-        # If there are changes on edge:
-        if(length(change.rows) > 0) {
-            
-            # Compile all changes into edge label:
-            edge.labels[i] <- paste(paste(character.changes[change.rows, "Character"], ": ", character.changes[change.rows, "From"], " -> ", character.changes[change.rows, "To"], sep = ""), collapse = "\n")
-            
-        }
-        
+    # If there are changes on edge:
+    if(length(change.rows) > 0) {
+      
+      # Compile all changes into edge label:
+      edge.labels[i] <- paste(paste(character.changes[change.rows, "Character"], ": ", character.changes[change.rows, "From"], " -> ", character.changes[change.rows, "To"], sep = ""), collapse = "\n")
+      
     }
     
-    # ADD DOT DOT DOT.....
-    
-    # Plot tree:
-    plot(tree, direction = "upwards")
-    
-    # Add edge labels for changes:
-    edgelabels(text = edge.labels, bg = "white", cex = 0.3)
-
-# NEED TO LADDERISE LEFT IF WRITING ON RIGHT OF BRANCHES...
-
+  }
+  
+  # ADD DOT DOT DOT.....
+  
+  # Plot tree:
+  plot(tree, direction = "upwards")
+  
+  # Add edge labels for changes:
+  edgelabels(text = edge.labels, bg = "white", cex = 0.3)
+  
+  # NEED TO LADDERISE LEFT IF WRITING ON RIGHT OF BRANCHES...
+  
 }
