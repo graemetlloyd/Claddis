da0c4960dee666b837b2f32b0d3fec4302c8db1a
Graeme T. Lloyd <graemetlloyd@gmail.com>
2020-08-03 18:02:32 +0100

    Styler code.

--- R/read_nexus_matrix.R
+++ R/read_nexus_matrix.R
@@ -31,16 +31,19 @@
 #' Maddison, D. R., Swofford, D. L. and Maddison, W. P., 1997. NEXUS: an extensible file format for systematic information. \emph{Systematic Biology}, \bold{46}, 590-621.
 #'
 #' @examples
-#' 
-#' # Create example matrix (NB: creates a file in the current
-#' # working directory called "morphmatrix.nex"):
-#' cat("#NEXUS\n\nBEGIN DATA;\n\tDIMENSIONS  NTAX=5 NCHAR=5;\n\t
-#' FORMAT SYMBOLS= \" 0 1 2\" MISSING=? GAP=- ;\nMATRIX\n\n
-#' Taxon_1  010?0\nTaxon_2  021?0\nTaxon_3  02111\nTaxon_4  011-1
-#' \nTaxon_5  001-1\n;\nEND;\n\nBEGIN ASSUMPTIONS;\n\t
-#' OPTIONS  DEFTYPE=unord PolyTcount=MINSTEPS ;\n\t
-#' TYPESET * UNTITLED  = unord: 1 3-5, ord: 2;\n\t
-#' WTSET * UNTITLED  = 1: 2, 2: 1 3-5;\nEND;", file = "morphmatrix.nex")
+#'
+#' # Create example matrix
+#' example_matrix <- paste0("#NEXUS\n\nBEGIN DATA;\n\tDIMENSIONS",
+#'   "  NTAX=5 NCHAR=5;\n\tFORMAT SYMBOLS= \" 0 1 2\" MISSING=? ",
+#'   "GAP=- ;\nMATRIX\n\nTaxon_1  010?0\nTaxon_2  021?0\nTaxon_3  ",
+#'   "02111\nTaxon_4  011-1\nTaxon_5  001-1\n;\nEND;\n\nBEGIN ",
+#'   "ASSUMPTIONS;\n\tOPTIONS  DEFTYPE=unord PolyTcount=MINSTEPS ",
+#'   ";\n\tTYPESET * UNTITLED  = unord: 1 3-5, ord: 2;\n\t",
+#'   "WTSET * UNTITLED  = 1: 2, 2: 1 3-5;\nEND;")
+#'
+#' # Write example matrix to current working directory called
+#' # "morphmatrix.nex":
+#' cat(example_matrix, file = "morphmatrix.nex")
 #' 
 #' # Read in example matrix:
 #' morph.matrix <- read_nexus_matrix("morphmatrix.nex")
