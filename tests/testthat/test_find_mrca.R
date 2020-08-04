e9ccbb830e7a3c1097a0f3195a7dcfd965fc5907
Graeme T. Lloyd <graemetlloyd@gmail.com>
2020-08-04 17:32:07 +0100

    First addition of testthat.

---
+++ tests/testthat/test_find_mrca.R
@@ -0,0 +1,9 @@
+library(testthat)
+library(Claddis)
+
+test_that("find_mrca returns correct node numbers", {
+  expect_equal(find_mrca(descendant_names = c("B", "C", "D"), tree = ape::read.tree(text = "(A,(B,(C,D)));")), 6)
+  expect_equal(find_mrca(descendant_names = c("A", "D"), tree = ape::read.tree(text = "(A,(B,(C,D)));")), 5)
+  expect_equal(find_mrca(descendant_names = c("C", "D"), tree = ape::read.tree(text = "(A,(B,(C,D)));")), 7)
+})
+
