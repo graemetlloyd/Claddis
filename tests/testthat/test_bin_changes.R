915a5eaf096f16793b4880f930873d0517c2026c
Graeme T. Lloyd <graemetlloyd@gmail.com>
2020-08-04 17:44:39 +0100

    Added unit test for bin_changes.

---
+++ tests/testthat/test_bin_changes.R
@@ -0,0 +1,7 @@
+library(testthat)
+library(Claddis)
+
+test_that("bin_changes returns correct bin counts", {
+  expect_equal(bin_changes(change_times = c(1.5, 1.5, 1.5, 2.5, 2.5, 2.5, 3.5, 3.5, 3.5), time_bins = c(4, 3, 2, 1)), c("4-3" = 3, "3-2" = 3, "2-1" = 3))
+  expect_equal(bin_changes(change_times = c(1, 1.5, 2, 2.5, 3, 3.5, 4), time_bins = c(4, 3, 2, 1)), c("4-3" = 2, "3-2" = 2, "2-1" = 2))
+})
--- tests/testthat/test_find_mrca.R
+++ tests/testthat/test_find_mrca.R
@@ -6,4 +6,3 @@ test_that("find_mrca returns correct node numbers", {
   expect_equal(find_mrca(descendant_names = c("A", "D"), tree = ape::read.tree(text = "(A,(B,(C,D)));")), 5)
   expect_equal(find_mrca(descendant_names = c("C", "D"), tree = ape::read.tree(text = "(A,(B,(C,D)));")), 7)
 })
-
