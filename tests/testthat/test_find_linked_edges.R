5da6587a5997e8ea7d53fef18982c0947bccd47f
Graeme T. Lloyd <graemetlloyd@gmail.com>
2020-08-04 17:51:37 +0100

    Added unit test for find_linked_edges.

---
+++ tests/testthat/test_find_linked_edges.R
@@ -0,0 +1,7 @@
+library(testthat)
+library(Claddis)
+
+test_that("find_linked_edges returns correct matrix", {
+  expect_equal(find_linked_edges(tree = ape::read.tree(text = "(A,(B,(C,D)));")), matrix(data = c(0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0), ncol = 6, byrow = TRUE, dimnames = list(1:6, 1:6)))
+})
+
