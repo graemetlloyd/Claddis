214d3b219606cbb9c14784d01a2a8f5071099235
Graeme T. Lloyd <graemetlloyd@gmail.com>
2020-08-06 17:29:54 +0100

    Added set.seed fix to end of test files.

--- tests/testthat/test_bin_changes.R
+++ tests/testthat/test_bin_changes.R
@@ -5,3 +5,6 @@ test_that("bin_changes returns correct bin counts", {
   expect_equal(bin_changes(change_times = c(1.5, 1.5, 1.5, 2.5, 2.5, 2.5, 3.5, 3.5, 3.5), time_bins = c(4, 3, 2, 1)), c("4-3" = 3, "3-2" = 3, "2-1" = 3))
   expect_equal(bin_changes(change_times = c(1, 1.5, 2, 2.5, 3, 3.5, 4), time_bins = c(4, 3, 2, 1)), c("4-3" = 2, "3-2" = 2, "2-1" = 2))
 })
+
+# Undo any set.seed usage:
+set.seed(Sys.time())
--- tests/testthat/test_find_linked_edges.R
+++ tests/testthat/test_find_linked_edges.R
@@ -4,3 +4,6 @@ library(Claddis)
 test_that("find_linked_edges returns correct matrix", {
   expect_equal(find_linked_edges(tree = ape::read.tree(text = "(A,(B,(C,D)));")), matrix(data = c(0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0), ncol = 6, byrow = TRUE, dimnames = list(1:6, 1:6)))
 })
+
+# Undo any set.seed usage:
+set.seed(Sys.time())
