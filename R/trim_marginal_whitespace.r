6d8b432a198903b9f26e819b62767fd83bcb23b2
Graeme T. Lloyd <graemetlloyd@gmail.com>
2020-09-15 16:29:08 +0100

    Fixing bug in trim_marginal_whitespace.

--- R/trim_marginal_whitespace.r
+++ R/trim_marginal_whitespace.r
@@ -2,17 +2,17 @@
 #'
 #' @description
 #'
-#' Trims any marginal whitespace from a character string.
+#' Trims any marginal whitespace from a vector of character string(s).
 #'
 #' @param x A character string
 #'
 #' @details
 #'
-#' Trims any marginal whitespace (spaces or tabs) from a character string.
+#' Trims any marginal whitespace (spaces or tabs) from a vector of character string(s).
 #'
 #' @return
 #'
-#' A character string with any leading or trailing whitespace removed.
+#' A vector of character string(s) with any leading or trailing whitespace removed.
 #'
 #' @author Graeme T. Lloyd \email{graemetlloyd@@gmail.com}
 #'
@@ -27,30 +27,46 @@
 #' @export trim_marginal_whitespace
 trim_marginal_whitespace <- function(x) {
   
-  # As long as the string has positive length:
-  if (nchar(x = x) > 0) {
+  # Make function to work on a scalar (single string):
+  trim_scalar <- function(x) {
     
-    # Split string into individual characters/whitespace:
-    split_string <- strsplit(x = x, split = "")[[1]]
-    
-    # Find positions of any whitespace:
-    position_is_whitespace <- apply(X = rbind(split_string == " ", split_string == "\t"), MARGIN = 2, FUN = any)
-    
-    # Find first non-whitesapce position:
-    first_non_qwhitespace_position <- min(which(position_is_whitespace == FALSE))
-    
-    # Find last non-whitesapce position:
-    last_non_whitespace_position <- max(which(position_is_whitespace == FALSE))
-    
-    # Return trimmed string:
-    return(value = paste(split_string[first_non_qwhitespace_position:last_non_whitespace_position], collapse = ""))
-  
-  # If string has no length:
-  } else {
-    
-    # Return string as is:
-    return(value = x)
+    # As long as the string has positive length:
+    if (nchar(x = x) > 0) {
+      
+      # Split string into individual characters/whitespace:
+      split_string <- strsplit(x = x, split = "")[[1]]
+      
+      # Find positions of any whitespace:
+      position_is_whitespace <- apply(X = rbind(split_string == " ", split_string == "\t"), MARGIN = 2, FUN = any)
+      
+      # If everything is whitespace:
+      if (all(position_is_whitespace)) {
+        
+        # Return empty string:
+        return("")
+        
+        # If at least one character is not whitespace:
+      } else {
+        
+        # Find first non-whitesapce position:
+        first_non_whitespace_position <- min(which(position_is_whitespace == FALSE))
+        
+        # Find last non-whitesapce position:
+        last_non_whitespace_position <- max(which(position_is_whitespace == FALSE))
+        
+        # Return trimmed string:
+        return(value = paste(split_string[first_non_whitespace_position:last_non_whitespace_position], collapse = ""))
+      }
+      
+      # If string has no length:
+    } else {
+      
+      # Return string as is:
+      return(value = x)
+    }
   }
-
+  
+  # Return output with marginal whitesapce pruned:
+  unlist(x = lapply(X = as.list(x = x), FUN = trim_scalar))
 }
 
