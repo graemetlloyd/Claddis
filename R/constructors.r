090208d4f22c1d693060f2e4b21fba399f159e15
Graeme T. Lloyd <graemetlloyd@gmail.com>
2020-09-24 14:43:36 +0100

    COmmenting out constructors file as currently breask things.

--- R/constructors.r
+++ R/constructors.r
@@ -1,5 +1,5 @@
-#' @export
-new_cladisticMatrix <- function(x = list()) {
-  
-  structure(x, class = "cladisticMatrix")
-}
+##' @export
+#new_cladisticMatrix <- function(x = list()) {
+#
+#  structure(x, class = "cladisticMatrix")
+#}
