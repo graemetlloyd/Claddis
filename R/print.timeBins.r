31242a0b4ad7f8f7276799e1cb862f8c42e30b13
Graeme T. Lloyd <graemetlloyd@gmail.com>
2020-10-11 14:44:19 +0100

    Fixing timeBins manual example widths.

--- R/print.timeBins.r
+++ R/print.timeBins.r
@@ -20,7 +20,15 @@
 #' @examples
 #'
 #' # Create a time bins object:
-#' time_bins <- matrix(data = c(99.6, 93.5, 93.5, 89.3, 89.3, 85.8, 85.8, 83.5, 83.5, 70.6, 70.6, 65.5), ncol = 2, byrow = TRUE, dimnames = list(c("Cenomanian", "Turonian", "Coniacian", "Santonian", "Campanian", "Maastrichtian"), c("fad", "lad")))
+#' time_bins <- matrix(
+#'   data = c(99.6, 93.5, 93.5, 89.3, 89.3, 85.8, 85.8, 83.5, 83.5, 70.6, 70.6, 65.5),
+#'   ncol = 2,
+#'   byrow = TRUE,
+#'   dimnames = list(
+#'     c("Cenomanian", "Turonian", "Coniacian", "Santonian", "Campanian", "Maastrichtian"),
+#'     c("fad", "lad")
+#'   )
+#' )
 #'
 #' # Set class as timeBins:
 #' class(time_bins) <- "timeBins"
