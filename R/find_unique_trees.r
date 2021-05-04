87c76892235ff0d224f75f3f482c04c8653a3758
Graeme T. Lloyd <graemetlloyd@gmail.com>
2021-05-04 12:28:03 +0100

    First additokn of find_unique_trees function.

---
+++ R/find_unique_trees.r
@@ -0,0 +1,90 @@
+#' Finds only the unique topologies amongst a set
+#'
+#' @description
+#'
+#' Given a set of trees with the saem tip labels, returns just the unique topologies present.
+#'
+#' @param trees An object of class \code{multiPhylo}.
+#'
+#' @details
+#'
+#' Where labelled topologies are generated randomly or modified by (e.g.) removing a tip, it may be useful to isolate just those that are truly unique. The \code{ape} package already has a function for this (\link{unique.multiPhylo}), but it can be slow when the number of trees is large. This function is thus intended as a faster version.
+#'
+#' The function works by breaking down a tree into its' component bipartitions and treating the combination of these as the definition of the tree. It thus escapes problems due to the principle of free rotation. Specifically, these two trees are actually identical:
+#'
+#' A  B  C   D  E
+#'  \/    \   \/
+#'   \     \  /
+#'    \     \/
+#'     \    /
+#'      \  /
+#'       \/
+#'
+#' B  A  D  E   C
+#'  \/    \/   /
+#'   \     \  /
+#'    \     \/
+#'     \    /
+#'      \  /
+#'       \/
+#'
+#' This ecomes clearer if we decompose them into their bipartitions:
+#'
+#' AB, DE, CDE, ABCDE
+#'
+#' These correspond to the decsendnats of each internal node (branching point) and the last one is actually ignored (the root node) as it will be present in any tree.
+#'
+#' @author Graeme T. Lloyd \email{graemetlloyd@@gmail.com}
+#'
+#' @return
+#'
+#' An object of class \code{"multiPhylo"}.
+#'
+#' @seealso
+#'
+#' \link{unique.multiPhylo}
+#'
+#' @examples
+#'
+#' # Make a set of three identical trees (differing only in "rotation" of nodes):
+#' trees <- ape::read.tree(text = c(
+#'   "((A,B),(C,(D,E)));",
+#'   "((C,(D,E)),(A,B));",
+#'   "((B,A),(C,(E,D)));")
+#' )
+#'
+#' # Show that there is only one unique tree:
+#' find_unique_trees(trees = trees)
+#'
+#' @export find_unique_trees
+find_unique_trees <- function(trees) {
+    
+  # Checks to add:
+  # - trees should be multiPhylo
+  # - trees should have same tip labels
+    
+  # Get number of tips (assumes all trees have same tips):
+  n_tips <- ape::Ntip(trees[[1]])
+    
+  # Return just unique topologies:
+  trees[!duplicated(
+    x = unlist(
+      x = lapply(
+        X = trees,
+        FUN = function(tree) {
+          paste(sort(
+            x = unlist(x = lapply(
+              X = as.list(x = n_tips + 2:tree$Nnode),
+              FUN = function(node) {
+                paste(sort(x = tree$tip.label[strap::FindDescendants(
+                  n = node,
+                  tree = tree
+                )]), collapse = "%%")
+              }
+            ))
+          ), collapse = "&&")
+        }
+      )
+    )
+  )]
+}
