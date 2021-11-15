df74057ffc718237e923a0f018528732705dc36a
Graeme T. Lloyd <graemetlloyd@gmail.com>
2021-11-15 22:14:17 +0000

    Filling out special case solutions for find_stepmatrix_minimum_span.

--- R/find_stepmatrix_minimum_span.r
+++ R/find_stepmatrix_minimum_span.r
@@ -8,11 +8,35 @@
 #'
 #' @details
 #'
-#' The minimum parsimony length a phylogenetic hypothesis could have depends on the stepmatrix of transition costs and the states actually sampled. If the stepmatrix rows and columns already represent sampled states then this minimum length is reduced to a graph theory problem - the minimum spanning tree. This function returns the length of that tree and hence the minimum steps value (as used in stratocladistics to define "parsimony debt" Fisher 1994, or in homoplasy indices, see Hoyal Cuthill 2015).
+#' The minimum parsimony length a phylogenetic hypothesis can have depends on the stepmatrix of transition costs and the states actually sampled. If the stepmatrix rows and columns already represent sampled states (the assumption here) then this minimum length is reduced to a graph theory problem - the minimum spanning tree. This function returns the length of that tree and hence the minimum cost (as used in stratocladistics to help define "parsimony debt" Fisher 1994, or in homoplasy indices, see Hoyal Cuthill 2015).
 #'
-#' Strictly speaking the answer depends on whether the stepmatrix is symmetric (representing an undirected graph) or asymmetric (representing a directed graph). In both cases the transition costs are formally the edge weights. If the former then the problem is the "regular" minimum spanning tree, and if the latter then it is known as the arboresence. If the former, then Kruskal's (1956) algorithm is used and if the latter then Edmonds' (1967) algorithm is used. (The function automatically makes this choice using the value of \code{stepmatrix$symmetry}.) In practice the implementations used come from the optrees package (Fontenla 2014), and specifically the functions \code{getMinimumSpanningTree} and \code{msArborEdmonds}.
+#' \bold{Special case solutions}
 #'
-#' Note that this function only returns the length of the answer (in steps), not the actual path.
+#' In order for the function to run as fast as possible the following special case solutions are used.
+#'
+#' \emph{Invariant character solution}
+#'
+#' If the character is invariant then the stepmatrix will have size one (one-by-one), and more simply the minimum spanning tree is length zero as there re no transitions to make.
+#'
+#' \emph{Unordered character solution}
+#'
+#' If the character is unordered all off-diagonal values of the stepmatrix are one and the minimum steps is the size of the stepmatrix minus one (as there is no cost to visit the root state).
+#'
+#' \emph{Ordered/irreversible/stratigraphic character solution}
+#'
+#' For an ordered, irreversible, or stratigraphic character the solution is also simple, and identical. This is the cost to go between the furthest two states which is simply the value in the upper right of the stepmatrix.
+#'
+#' \emph{Dollo character solution}
+#'
+#' Despite other complexities Dollo characters also have a simple solution that mirrors that for ordered, irreversible, or stratigraphic characters. I.e., the derived states must all be acquired once which is simply the value in the lower left of the stepmatrix.
+#'
+#' \bold{General case solution}
+#'
+#' For characters that do not constitute any of the above special case solutions a slower running general case solution is required. This is effectively limited to so-called "custom" characters - \code{stepmatrix$type = "custom"} that can only be represented by either a state tree or stepmatrix.
+#'
+#' Strictly speaking the solution used depends on whether the stepmatrix is symmetric (representing an undirected graph, or simply a graph) or asymmetric (representing a directed graph, or digraph). In both cases the edge weights of the graph/digraph are the transition costs from the stepmatrix. For a graph the problem is the "regular" minimum spanning tree, and if for a digraph then it is the arboresence. For regular minimum spanning trees Kruskal's (1956) algorithm is used and for aboresence's Edmonds' (1967) algorithm is used. (The function automatically makes this choice using the value of \code{stepmatrix$symmetry}.) In practice the implementations used come from the optrees package (Fontenla 2014), and specifically the functions \code{getMinimumSpanningTree} and \code{msArborEdmonds}.
+#'
+#' Note that regardless of method the function only returns the \emph{length} of the minimum spanning tree or arboresence (i.e., the total minimum cost), not the actual path (pattern of states traversed). Similarly, multiple paths (trees or arboresences) may exist that have this minimum length.
 #'
 #' @author Graeme T. Lloyd \email{graemetlloyd@@gmail.com}
 #'
@@ -55,9 +79,21 @@
 find_stepmatrix_minimum_span <- function(stepmatrix) {
   
   # ADD SOME DATA CHECKS?
+
+  # Things are simple if character is invariant (one-by-one matrix) - there are no changes:
+  if (stepmatrix$size == 1) return(0)
+
+  # Things are simple if is an unordered character (there must minimally be N - 1 links):
+  if (stepmatrix$type == "unordered") return(stepmatrix$size - 1)
+  
+  # Things are also simple if it is an ordered, irreversible or stratigraphic character (top right value is minimum steps):
+  if (stepmatrix$type == "ordered" || stepmatrix$type == "irreversible" || stepmatrix$type == "stratigraphy") return(stepmatrix$stepmatrix[1, stepmatrix$size])
+  
+  # Things are simple if is an Dollo character (lower left value is minimum steps):
+  if (stepmatrix$type == "dollo") return(stepmatrix$stepmatrix[stepmatrix$size, 1])
   
   # Set sampled states and stepmatrix rows and columns to simple numbers to work with optrees:
-  sampled_states <- rownames(x = stepmatrix$stepmatrix) <- colnames(x = stepmatrix$stepmatrix)  <- 1:stepmatrix$size
+  sampled_states <- rownames(x = stepmatrix$stepmatrix) <- colnames(x = stepmatrix$stepmatrix) <- 1:stepmatrix$size
   
   # Build matrix of shortest incoming edge(s) (those available for the aboresence):
   available_edges <- do.call(
@@ -66,7 +102,7 @@ find_stepmatrix_minimum_span <- function(stepmatrix) {
       X = as.list(x = sampled_states),
       FUN = function(state) {
         cbind(
-          from = names(x = which(x = stepmatrix$stepmatrix[, state] == min( x = stepmatrix$stepmatrix[-which(x = rownames(x = stepmatrix$stepmatrix) == state), state]))),
+          from = names(x = which(x = stepmatrix$stepmatrix[, state] == min(x = stepmatrix$stepmatrix[-which(x = rownames(x = stepmatrix$stepmatrix) == state), state]))),
           to = state
         )
       }
